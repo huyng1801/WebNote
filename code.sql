fe5df8010dcafd5340c4a9285a73c075f7993808
huynguyendev1801 <huynguyendev18012003@gmail.com>
2024-03-22 06:03:50 +0700

    Initial commit

---
+++ code.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE WebNote;
+GO
+USE WebNote;
+GO
+CREATE TABLE NguoiDung(
+    MaNguoiDung INT PRIMARY KEY IDENTITY,
+    TenNguoiDung NVARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    MatKhau VARCHAR(100) NOT NULL
+);
+GO
+CREATE TABLE GhiChu(
+    MaGhiChu INT PRIMARY KEY IDENTITY,
+    TieuDe NVARCHAR(255) NOT NULL,
+    NoiDung NTEXT NULL,
+    NgayTaoGhiChu DATETIME DEFAULT GETDATE(),
+    MaNguoiDung INT,
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung)
+);
+GO
+CREATE TABLE Congviec(
+    MaCongViec INT PRIMARY KEY IDENTITY,
+    TieuDe NVARCHAR(255) NOT NULL,
+    TrangThai NVARCHAR(20) CHECK (TrangThai IN (N'Hoàn thành', N'Đang thực hiện', N'Đã hủy')),
+    MaNguoiDung INT,
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung)
+); 
+GO
+CREATE TABLE SuKien(
+    MaSuKien INT PRIMARY KEY IDENTITY,
+    TieuDe NVARCHAR(255) NOT NULL,
+    ThoiGianBatDau DATETIME,
+    ThoiGianKetThuc DATETIME,
+    MaNguoiDung INT,
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung)
+);
+GO
